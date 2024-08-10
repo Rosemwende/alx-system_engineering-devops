9aaff10bd2a208c28ed7a8b59902bbb3e5930866
Rosemwende <rosemuema82@gmail.com>
2024-08-10 11:21:51 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,4 @@
 #!/usr/bin/pup
-# This Puppet manifest installs Flask version 2.1.0 using pip3
-
 # Ensure that Python 3 and pip3 are installed
 package { 'python3-pip':
   ensure => installed,
@@ -11,4 +9,11 @@ exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin', '/usr/local/bin'],
   unless  => '/usr/bin/pip3 show Flask | grep "Version: 2.1.0"',
+}
+
+# Install Werkzeug version 2.1.0 using pip3
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show Werkzeug | grep "Version: 2.1.0"',
 }
