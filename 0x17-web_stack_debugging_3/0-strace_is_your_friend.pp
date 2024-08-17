4cd2d521ada024151ca9f84c4c0a462361ffe97b
Rosemwende <rosemuema82@gmail.com>
2024-08-17 14:06:34 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # This Puppet manifest fixes the issue causing Apache to return a 500 error.
-# by editing the mistyped .phpp to php in the var/www/html/wp-settings.php file
+# by editing the mistyped .phpp to php in the /var/www/html/wp-settings.php file
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g var/www/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/bin/:/bin/',
 }
