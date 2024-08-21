4dc0922102726b952fb69fe3af53092ca5c2c728
Rosemwende <rosemuema82@gmail.com>
2024-08-21 12:41:32 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
-""" Python script that, using this REST API, 
- for a given employee ID, 
- returns information about his/her TODO list progress """
+"""Python script that, using this REST API,
+for a given employee ID,
+returns information about his/her TODO list progress.
+"""
 
 import requests
 from sys import argv
-""" The modules to work with """
+
+"""The modules to work with"""
+
 
 def get_employee_todos_progress(employee_id):
-	""" A function about the employee todos progress to return the info """
-	try:
-	    url = "https://jsonplaceholder.typicode.com/"
-	    user_datas = requests.get(url + f"users/{employee_id}")
-	    user_data = user_datas.json()
-	    employee_name = user_data["name"]
-	 
-	"""fetch the employees todos list"""
-	todos_list = requests.get(url + f"todos?userId={employee_id}")
-	json_todos_list = todos_list.json()
+    """A function about the employee todos progress to return the info"""
+    try:
+        url = "https://jsonplaceholder.typicode.com/"
+        user_datas = requests.get(url + f"users/{employee_id}")
+        user_data = user_datas.json()
+        employee_name = user_data["name"]
 
-	total_task = len(json_todos_list)
-	task_done = [task for task in json_todos_list if task['completed']]
-	no_task_done = len(task_done)
+        """fetch the employee's todos list"""
+        todos_list = requests.get(url + f"todos?userId={employee_id}")
+        json_todos_list = todos_list.json()
 
-	"""Didplay the result"""
-	print(f"Employee {employee_name} is done with tasks("
-	      f"{no_task_done}/{total_task}):")
-	
-	"""title of completed task"""
-	for task in task_done:
-		print(f"\t {task['title']}")
+        total_task = len(json_todos_list)
+        task_done = [task for task in json_todos_list if task['completed']]
+        no_task_done = len(task_done)
+
+        """Display the result"""
+        print(f"Employee {employee_name} is done with tasks("
+              f"{no_task_done}/{total_task}):")
+
+        """Title of completed task"""
+        for task in task_done:
+            print(f"\t {task['title']}")
     except Exception as e:
-	print(f"an error occured: {e}")
+        print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-	if len (argv) != 2:
-		print("Usage: Script <employee_id>")
-	else:
-		get_employee_todos_progress(argv[1])
+    if len(argv) != 2:
+        print("Usage: Script <employee_id>")
+    else:
+        get_employee_todos_progress(argv[1])
+
