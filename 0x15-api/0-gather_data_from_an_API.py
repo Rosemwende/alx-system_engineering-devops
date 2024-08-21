a6dc448fdddb50f05ce16a4d2d59d97464fe8d22
Rosemwende <rosemuema82@gmail.com>
2024-08-21 13:54:42 +0000

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -14,12 +14,12 @@ def get_employee_todos_progress(employee_id):
     """A function about the employee todos progress to return the info"""
     try:
         url = "https://jsonplaceholder.typicode.com/"
-        user_datas = requests.get(url + f"users/{employee_id}")
+        user_datas = requests.get("{}users/{}".format(url, employee_id))
         user_data = user_datas.json()
-        employee_name = user_data["name"]
+        employee_name = user_data['name']
 
         """fetch the employee's todos list"""
-        todos_list = requests.get(url + f"todos?userId={employee_id}")
+        todos_list = requests.get("{}todos?userId={}".format(url, employee_id))
         json_todos_list = todos_list.json()
 
         total_task = len(json_todos_list)
@@ -27,14 +27,13 @@ def get_employee_todos_progress(employee_id):
         no_task_done = len(task_done)
 
         """Display the result"""
-        print(f"Employee {employee_name} is done with tasks("
-              f"{no_task_done}/{total_task}):")
+        print("Employee {} is done with tasks ({}/{})".format(employee_name, no_task_done, total_task))
 
         """Title of completed task"""
         for task in task_done:
-            print(f"\t {task['title']}")
+            print("\t {}".format(task['title']))
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print("An error occurred: {}".format(e))
 
 
 if __name__ == "__main__":
